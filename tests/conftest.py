f66a2c9a8144402693606d696b89bf87a8417479
S.M. Faisal Ahmed <100210147+sm-faisalahmed@users.noreply.github.com>
2026-06-15 19:04:46 +0600

    Add tests/conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from playwright.sync_api import sync_playwright, Browser, Page
+
+
+@pytest.fixture(scope="session")
+def browser() -> Browser:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        yield browser
+        browser.close()
+
+
+@pytest.fixture(scope="function")
+def page(browser: Browser) -> Page:
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
